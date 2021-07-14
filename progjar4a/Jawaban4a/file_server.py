d7424363c6b2f91547001592544d5473a11c5673
natihdevayanti <56763600+natihdevayanti@users.noreply.github.com>
2021-07-15 00:44:07 +0800

    Update file_server.py

--- progjar4a/Jawaban4a/file_server.py
+++ progjar4a/Jawaban4a/file_server.py
@@ -30,7 +30,7 @@ class ProcessTheClient(threading.Thread):
 
 
 class Server(threading.Thread):
-    def __init__(self,ipaddress='0.0.0.0',port=8889):
+    def __init__(self,ipaddress='192.168.122.45',port=5005):
         self.ipinfo=(ipaddress,port)
         self.the_clients = []
         self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -51,7 +51,7 @@ class Server(threading.Thread):
 
 
 def main():
-    svr = Server(ipaddress='0.0.0.0',port=6666)
+    svr = Server(ipaddress='192.168.122.45',port=5005)
     svr.start()
 
 
